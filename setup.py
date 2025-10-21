80330b560ac96bbc5928635270c5805b7276a79d
swethadevanapalli <swethadevanapalli151@gmail.com>
2025-10-21 11:44:10 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Swetha Devanapalli",
+    author_email="swethadevanapalli151@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
